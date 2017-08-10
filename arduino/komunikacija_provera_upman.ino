e01218d6cfefff191da9d5cf7f8f3576b56cc406
ilijamikic <30274157+ilijamikic@users.noreply.github.com>
2017-08-10 13:35:29 +0200

    Add files via upload

---
+++ arduino/komunikacija_provera_upman.ino
@@ -0,0 +1,25 @@
+
+int sum = 0;
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+}
+
+void loop() {
+  byte upper = Serial.read();
+  byte lower = Serial.read();
+  int x = (upper<<8) | lower; //Reassemble the number
+  Serial.print(x, DEC);
+  
+  if(x>500){
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);
+  }
+  else if(x<500){
+  digitalWrite(LED_BUILTIN, LOW);delay(1000);
+  }
+   
+  
+   
+}
